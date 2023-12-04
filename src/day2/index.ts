09bc69f10c55493693db502d225b494628203ab4
Patrick Long <patrick@deepspacerobots.com>
2023-12-04 11:34:07 -0600

    create script to run all .ts files and log more clearly

---
+++ src/day2/index.ts
@@ -0,0 +1,19 @@
+// @ts-ignore
+import fs from "fs";
+
+console.log("Day 2:");
+
+export default function main() {
+	function partOne() {
+		console.log("Part 1:");
+	}
+
+	function partTwo() {
+		console.log("Part 2:" + "\n");
+	}
+
+	partOne();
+	partTwo();
+}
+
+main();
